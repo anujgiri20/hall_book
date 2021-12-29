533e4646d673c60eb3a5eabdc0be9248c38fdd19
anujgiri20 <87684592+anujgiri20@users.noreply.github.com>
2021-12-29 12:27:06 +0700

    first commit

---
+++ customers.js
@@ -0,0 +1,6 @@
+let customers = [
+    { id: 1, name: "aniket", address: "nagpur", isActive: true },
+    { id: 2, name: "ganesh", address: "Mumbai", isActive: false },
+  ];
+  
+  module.exports = customers;
